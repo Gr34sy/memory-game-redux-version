1f018d02210918b6ec7f32701c17c9e48fdd0c1a
Piotr Słupski <piotr.slupski17@gmail.com>
2024-08-23 13:07:36 +0200

    finished button component

--- src/components/components/button/Button.tsx
+++ src/components/components/button/Button.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { PropsWithChildren } from "react";
+import { ComponentPropsWithoutRef } from "react";
+import styles from "./button.module.css";
 
-const Button = () => {
-  return <div></div>;
+type ButtonProps = Omit<ComponentPropsWithoutRef<"button">, "className"> & {
+  buttonType: "primary" | "secondary" | "selection" | "menu";
+  isActive?: boolean;
+};
+
+const Button = ({
+  children,
+  buttonType,
+  isActive,
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  return (
+    <button
+      {...props}
+      className={`${styles.button} ${styles[`button--${buttonType}`]} ${isActive ? styles.active : ""}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/pages/homepage/Homepage.tsx
+++ src/components/pages/homepage/Homepage.tsx
@@ -1,9 +1,16 @@
+import Button from "../../components/button/Button";
 import styles from "./homepage.module.css";
 
 const Homepage = () => {
   return (
     <main className={styles.homepage}>
       <div>Home</div>
+
+      <div className={styles["test-wrapper"]}>
+        <Button buttonType="primary" onClick={() => console.log("click")}>
+          Test
+        </Button>
+      </div>
     </main>
   );
 };
