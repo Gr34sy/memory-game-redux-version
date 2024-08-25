0df206b2b066e74f719be95d53dd9244b9c39d65
Piotr Słupski <piotr.slupski17@gmail.com>
2024-08-25 12:07:34 +0200

    created overlay using react portal

--- src/components/components/overlay/Overlay.tsx
+++ src/components/components/overlay/Overlay.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+// styles
+import styles from "./overlay.module.css";
+// types
+import { PropsWithChildren } from "react";
 
-const Overlay = () => {
-  return <div></div>;
+// hooks
+import { useEffect, useRef, MutableRefObject } from "react";
+import { createPortal } from "react-dom";
+
+type elRef = MutableRefObject<HTMLDivElement | null>;
+
+const Overlay = ({ children }: PropsWithChildren) => {
+  const elRef: elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const portalRoot = document.getElementById("portal");
+    if (!portalRoot || !elRef.current) {
+      return;
+    }
+    portalRoot.appendChild(elRef.current);
+
+    return () => {
+      if (elRef.current) {
+        portalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+
+  return createPortal(
+    <div className={styles.overlay}>{children}</div>,
+    elRef.current
+  );
 };
 
 export default Overlay;
